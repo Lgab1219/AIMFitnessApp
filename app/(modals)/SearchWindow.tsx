140cf2cbabf986aae57a4631f1426ff79ccf2cfd
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-12-09 18:07:14 +0800

    configured USDA to fetch food data, calories cannot be updated yet

--- app/(modals)/SearchWindow.tsx
+++ app/(modals)/SearchWindow.tsx
@@ -1,9 +1,41 @@
 import { useRouter } from "expo-router";
+import { useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, View } from "react-native";
+
+type Food = [{
+  id?: number,
+  name?: string,
+  calories?: number
+}]
 
 export default function SearchWindow() {
 
     const router = useRouter();
+
+    // State holding the search query
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    // State holding the search results for viewing
+    const [searchResults, setSearchResults] = useState<Food>([{}]);
+
+    async function handleSearch(query: string) {
+
+      if (!query) {
+        return;
+      }
+
+      try {
+        // Client asks server for search results based on query
+        const res = await fetch(`http://192.168.1.6:3000/search?query=${encodeURIComponent(query)}`);
+
+        // Turn received response from server into JSON
+        const data = await res.json();
+
+        setSearchResults(data.foods);
+      } catch (error) {
+        console.error("Error fetching search results: ", error);
+      }
+    }
 
     return (
         <>
@@ -16,7 +48,26 @@ export default function SearchWindow() {
                 </View>
                 <View style={styles.titleContainer}>
                     <Text style={styles.subtitleStyle}>Search your food</Text>
-                    <TextInput style={{ borderColor: '#000000', borderWidth: 1, width: '100%', padding: 10 }} placeholder="Search..." />
+                    <TextInput 
+                    style={{ borderColor: '#000000', borderWidth: 1, width: '100%', padding: 10 }} 
+                    placeholder="Search..." 
+                    value={searchQuery}
+                    onChangeText={(text) => setSearchQuery(text)}/>
+                    <TouchableHighlight underlayColor='#e6733c' style={styles.buttonStyle}>
+                      <Text style={{ color: '#FFFFFF', textAlign: 'center', fontSize: 18, fontWeight: '600' }} onPress={() => handleSearch(searchQuery)}>Search</Text>
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.foodsContainer}>
+                    {searchResults ? searchResults.map((food) => (
+                      <>
+                      <TouchableHighlight style={styles.food} key={food.id} underlayColor= '#e6733c' onPress={() => {console.log("PRESSED")}}>
+                        <>
+                        <Text>{food.name}</Text>
+                        <Text>Calories: {food.calories}</Text>
+                        </>
+                      </TouchableHighlight> 
+                      </>
+                    )) : <Text>No results found.</Text>}
                 </View>
             </ScrollView>
         </>
@@ -24,11 +75,26 @@ export default function SearchWindow() {
 }
 
 const styles = StyleSheet.create({
+  food: {
+    flexDirection: 'column',
+    gap: 4,
+    backgroundColor: '#E0E0E0',
+    borderWidth: 1 || 0,
+    borderColor: '#000000',
+    paddingInline: 15,
+    paddingVertical: 10
+  },
+  foodsContainer: {
+    flexDirection: 'column',
+    gap: 12,
+    paddingInline: 20,
+    paddingVertical: 20,
+  },
   titleContainer: {
     flexDirection: 'column',
     gap: 8,
     backgroundColor: '#E0E0E0',
-    paddingInline: "5%"
+    paddingInline: "5%",
   },
 
   subtitleStyle: {
