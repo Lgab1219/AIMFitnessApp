fbb9bd120e0991320cf0e69b38b0ef67c2d5047e
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-12-13 22:26:34 +0800

    fixed: food data fetch bug, just some Typescript type issues

--- backend/api/usda.ts
+++ backend/api/usda.ts
@@ -3,6 +3,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+type USDANutrient = {
+    nutrientName: string;
+    unitName: string;
+    value: number;
+}
+
+type USDASearchResponse = {
+    foods?: USDAFood[];
+}
+
+type USDAFood = {
+    fdcId: number;
+    description: string;
+    foodNutrients?: USDANutrient[];
+};
+
+type SimplifiedFood = {
+  id: number;
+  name: string;
+  calories: number | null;
+};
+
+
 export default async function usda(req: VercelRequest, res: VercelResponse) {
 
     const query = req.query.query;
@@ -15,7 +38,25 @@ export default async function usda(req: VercelRequest, res: VercelResponse) {
 
     const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${usdaAPI}&query=${query}`);
 
-    const data = await response.json();
+    const data: USDASearchResponse = await response.json();
 
-    return res.status(200).json(data);
+      const filteredData: SimplifiedFood[] = (data.foods ?? [])
+    .filter((food: USDAFood) => Array.isArray(food.foodNutrients))
+    .map((food: USDAFood): SimplifiedFood => {
+      const calories = food.foodNutrients?.find(
+        (nutrient: USDANutrient) =>
+          nutrient.nutrientName === "Energy" &&
+          nutrient.unitName === "KCAL"
+      );
+
+      return {
+        id: food.fdcId,
+        name: food.description,
+        calories: calories?.value ?? null,
+      };
+    })
+    .filter((item: SimplifiedFood) => item.calories !== null)
+    .slice(0, 10);
+
+    return res.status(200).json(filteredData);
 }
