e5e972deaa4e99c4087bb7654dffd9b7a13e23ef
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-12-13 18:46:51 +0800

    changed backend from node servers into vercel serverless functions

---
+++ backend/api/gemini.ts
@@ -0,0 +1,22 @@
+import { GoogleGenAI } from '@google/genai';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async function gemini(req: VercelRequest, res: VercelResponse) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    const { message } = req.body;
+
+    const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+    const result = await ai.models.generateContent({
+        model: 'gemini-2.5-flash',
+        contents: message
+    })
+    
+    return res.status(200).json({ result });
+}
